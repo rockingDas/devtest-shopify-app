160e64831ee1c5dd50352de026bc48960f932f6d
Suprakash <rocking1999das@gmail.com>
2025-12-04 16:33:38 +0530

    Stop tracking sensitive includes/config.php

---
+++ includes/config_sample.php
@@ -0,0 +1,21 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+
+session_start();
+
+define("API_KEY", "");
+
+define("SECRECT_KEY", "");
+
+define("APP_URL", "");
+
+define("SCOPES", "");
+
+define("API_VERSION", "2025-01");
+
+define("API_FOLDER_NAME", "");
